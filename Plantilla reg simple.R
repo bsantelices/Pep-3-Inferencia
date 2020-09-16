bd44c4c73a457840a192d0c41d5eb6caf2d11fb5
Unknown <bryan.santelices@usach.cl>
2020-09-16 02:11:18 -0300

    arreglo seed

--- Plantilla reg simple.R
+++ Plantilla reg simple.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(ggpubr)
 library(lmtest)
 
-seed(100)
+set.seed(100)
 #########Cambiar titulos y variables##########
 
 # Se utiliza el grafico de dispersión con el fin de analizar visualmente si existe 
